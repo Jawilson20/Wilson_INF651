f3138e66c72021c12a17a9ea02b676dc98b53858
Jawilson20 <Wilsonjakolbi57@gmail.com>
2024-09-02 22:09:35 -0500

    in class challenges and Assignment 1

---
+++ in-class work/classworkmain.js
@@ -0,0 +1,12 @@
+let MyName0='Jakolbi';
+let MyAge='20';
+let IsStudent='yes'
+
+console.log('MyName:', MyName0, 'Type:', typeof MyName); 
+console.log('MyAge:', MyAge, 'Type:', typeof MyAge);   
+console.log('IsStudent:', IsStudent, 'Type:', typeof IsStudent);
+
+let Mymoney='$300'
+let Foodcost='$100'
+
+console.log
